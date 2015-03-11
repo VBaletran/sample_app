35325346649df95abf9c77905642b95ab3d86a81
VBaletran <vincent.baletran@me.com>
2015-03-11 18:19:56 +0100

    Fini avec les pages statiques

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'pages/home'
 
   get 'pages/contact'
+  get 'pages/about'
+  get 'pages/home'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
